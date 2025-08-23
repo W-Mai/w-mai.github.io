8e087b3ec0f08fca8791c675ef61703fac16c7ec
Benign X <1341398182@qq.com>
2025-08-23 16:05:22 +0800

    😁(init): init projects

---
+++ web/src/consts.ts
@@ -0,0 +1,27 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const USER_NAME = 'w-mai';
+export const SITE_TITLE = 'B3n1gn X';
+export const SITE_DESCRIPTION = 'B3n1gn X | Personal Website';
+
+export const SOCIALS = [
+    {
+        name: 'GitHub',
+        url: 'https://github.com/w-mai',
+        description: 'GitHub Homepage',
+        icon: '/social/github.svg',
+    },
+    {
+        name: 'Twitter',
+        url: 'https://twitter.com/_w_mai_',
+        description: 'Twitter Homepage',
+        icon: '/social/twitter.svg'
+    },
+    {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/b3n1gnx/',
+        description: 'LinkedIn Homepage',
+        icon: '/social/linkedin.svg'
+    },
+];
