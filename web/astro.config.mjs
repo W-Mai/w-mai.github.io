bb4881a912712ee041ae1b3ab7738f9cb0422416
Benign X <1341398182@qq.com>
2025-08-24 12:52:46 +0800

    😁(features): update Astro configuration and add expressive code integration

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,11 +3,17 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import {defineConfig, passthroughImageService} from 'astro/config';
+import {astroExpressiveCode, pluginFrames, pluginTextMarkers} from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://benign.host',
+    integrations: [astroExpressiveCode({
+        plugins: [
+            pluginFrames(),
+            pluginTextMarkers()
+        ],
+    }), mdx(), sitemap()],
     image: {
         service: passthroughImageService()
     }
